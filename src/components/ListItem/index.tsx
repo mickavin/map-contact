b2327c3d5119abcc339939191c244a96ff6cb240
Kevin Jean-Philippe <j-philippe.kevin@outlook.fr>
2022-09-19 12:01:43 +0200

    Ajouter adresse contact

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -9,7 +9,7 @@ interface ListItemProps {
 
 const ListItem = memo((props: ListItemProps) => {
   const { item, chooseInfo, removeContact } = props
-  const {type, name} = item
+  const {type, name, address} = item
 
   // supprime un contact sélectionné
   const removeThisContact = useCallback(() => {
@@ -22,15 +22,20 @@ const ListItem = memo((props: ListItemProps) => {
     <li className="flex items-start space-x-6 p-6">
       <div onClick={() => chooseInfo(item)}  className="min-w-0 relative flex-auto cursor-pointer">
         <h2 className="font-semibold text-slate-900 truncate pr-20 text-left">{name}</h2>
-        <dd>
+        <p>
           {type}
-        </dd>
+        </p>
+        <p className="text-truncate">
+          {address}
+        </p>
       </div>
-      <div 
-      onClick={removeThisContact}
-      className="rotate-45 bg-red-500 rounded-2xl w-5 h-5 justify-center items-center text-white ml-0 flex m-auto cursor-pointer" 
-      style={{fontSize: '14px'}}>
-          +
+      <div className="m-auto">
+        <div 
+        onClick={removeThisContact}
+        className="rotate-45 bg-red-500 rounded-2xl w-5 h-5 justify-center items-center text-white ml-0 flex m-auto cursor-pointer" 
+        style={{fontSize: '14px'}}>
+            +
+        </div>
       </div>
     </li>
   )
